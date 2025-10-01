b4ed5baf533334804bfe325d2888fde8b285962d
Shravankumar Janawade <shravanjanwade2252@gmail.com>
2025-10-01 18:24:59 -0400

    Completed assignment 2

--- app/Labs/Lab2/Positions.tsx
+++ app/Labs/Lab2/Positions.tsx
@@ -49,7 +49,7 @@ const Positions = () => {
       <div id="wd-css-position-fixed">
         <h2>Fixed position</h2>
         Checkout the blue square that says {"Fixed position"} stuck all the way
-        on the right and halfway down the page. It doesn't scroll with the rest
+        on the right and halfway down the page. It {"doesn't"} scroll with the rest
         of the page. Its position is {"Fixed"}.
         <div className="wd-pos-fixed wd-dimension-square wd-bg-color-blue wd-fg-color-white">
           Fixed position
