cfe8c59e3cad5f8e5fc6fd6b39592e33ce1d5430
Shravankumar Janawade <shravanjanwade2252@gmail.com>
2025-10-01 18:20:04 -0400

    Fixed the quotes

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -26,8 +26,8 @@ export default function Lab2() {
         <h3>Styling with the STYLE attribute</h3>
         <p style={{ backgroundColor: "blue", color: "white" }}>
           Style attribute allows configuring look and feel right on the element.
-          Although it's very convenient it is considered bad practice and you
-          should avoid using the style attribute
+          Although {"it's"} very convenient it is considered bad practice and
+          you should avoid using the style attribute
         </p>
 
         <div id="wd-css-id-selectors">
@@ -37,15 +37,16 @@ export default function Lab2() {
             same name, e.g., P, we can refer to a specific element by its ID
           </p>
           <p id="wd-id-selector-2">
-            Here's another paragraph using a different ID and a different look
-            and feel
+            {" Here's"} another paragraph using a different ID and a different
+            look and feel
           </p>
         </div>
         <div id="wd-css-class-selectors">
           <h3>Class selectors</h3>
 
           <p className="wd-class-selector">
-            Instead of using IDs to refer to elements, you can use an element's
+            Instead of using IDs to refer to elements, you can use an{" "}
+            {"element's"}
             CLASS attribute
           </p>
 
@@ -60,7 +61,7 @@ export default function Lab2() {
               Selectors can be combined to refer elements in particular places
               in the document
               <p className="wd-selector-3">
-                This paragraph's red background is referenced as
+                This {"paragraph's"} red background is referenced as
                 <br />
                 .selector-2 .selector3
                 <br />
